9e01f791b404cb29bff10c506a5bddfad5eef69e
sakura23326 <1590573969@qq.com>
2025-12-26 19:18:43 +0800

    猫猫移动
    cat

--- Assets/smallthings/sprite ( milk )/PlayerMovement.cs
+++ Assets/smallthings/sprite ( milk )/PlayerMovement.cs
@@ -1,4 +1,3 @@
-
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -7,34 +6,47 @@ public class PlayerMovement : MonoBehaviour
     public float jumpForce = 5f;
 
     private Rigidbody rb;
-    private bool isGrounded = true;
+    private bool isGrounded;
+    // --- 新增：声明动画组件变量 ---
+    private Animator anim;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        // --- 新增：获取动画组件 ---
+        anim = GetComponent<Animator>();
+
+        rb.freezeRotation = true;
+    }
+
+    void FixedUpdate()
+    {
+        float xMove = 0f;
+        float zMove = 0f;
+
+        if (Input.GetKey(KeyCode.S)) zMove = moveSpeed;
+        if (Input.GetKey(KeyCode.W)) zMove = -moveSpeed;
+        if (Input.GetKey(KeyCode.D)) xMove = -moveSpeed;
+        if (Input.GetKey(KeyCode.A)) xMove = moveSpeed;
+
+        Vector3 velocity = rb.velocity;
+        velocity.x = xMove;
+        velocity.z = zMove;
+        rb.velocity = velocity;
+
+        // --- 新增：处理动画逻辑 ---
+        // 只要 xMove 或 zMove 不为 0，就代表正在移动
+        bool isMoving = (xMove != 0 || zMove != 0);
+
+        // 只有当动画组件存在时才赋值，防止报错
+        if (anim != null)
+        {
+            anim.SetBool("isWalking", isMoving);
+        }
     }
 
     void Update()
     {
-        float moveX = 0f;
-        float moveZ = 0f;
-
-        // Z-axis movement (Forward / Backward)
-        if (Input.GetKey(KeyCode.S))
-            moveZ = 1f;
-        else if (Input.GetKey(KeyCode.W))
-            moveZ = -1f;
-
-        // X-axis movement (Left / Right)
-        if (Input.GetKey(KeyCode.A))
-            moveX = 1f;
-        else if (Input.GetKey(KeyCode.D))
-            moveX = -1f;
-
-        Vector3 movement = new Vector3(moveX, 0f, moveZ);
-        transform.Translate(movement * moveSpeed * Time.deltaTime, Space.World);
-
-        // Jump (Y-axis)
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
@@ -42,10 +54,9 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    // Detect ground
-    void OnCollisionEnter(Collision collision)
+    void OnCollisionStay(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Ground"))
+        if (collision.contacts[0].normal.y > 0.5f)
         {
             isGrounded = true;
         }
